27894e7b88655d41df8184360209c6f03c945689
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2021-10-14 14:05:59 -0300

    Iniciando um projeto em unity e aplicando no flutter. Interação do flutter refletindo em ações no unity.

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_unity_widget/flutter_unity_widget.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  UnityWidgetController? _unityWidgetController;
+  double _sliderValue = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text("Gustavo's project"),
+          centerTitle: true,
         ),
         body: Container(
           color: Colors.grey,
           child: Padding(
             padding: EdgeInsets.all(20),
-            child: Container(
-              color: Colors.white,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50.0),
+              ),
+              child: Stack(
+                children: [
+                  UnityWidget(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                    fullscreen: false,
+                    onUnityCreated: onUnityCreated,
+                    onUnitySceneLoaded: (message) => onUnitySceneLoaded,
+                    onUnityMessage: onUnityMessage,
+                  ),
+                  Positioned(
+                    bottom: 20,
+                    left: 20,
+                    right: 20,
+                    child: Card(
+                      elevation: 10,
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Text("Velocidade de rotação:"),
+                          ),
+                          Slider(
+                            onChanged: (value) {
+                              setState(() {
+                                _sliderValue = value;
+                              });
+                              setRotationSpeed(value.toString().replaceAll(".", ","));
+                            },
+                            value: _sliderValue,
+                            min: -20,
+                            max: 20,
+
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
   }
+
+  // Communcation from Flutter to Unity
+  void setRotationSpeed(String speed) {
+    _unityWidgetController?.postMessage(
+      'Cube',
+      'SetRotationSpeed',
+      speed,
+    );
+  }
+
+  // Communication from Unity to Flutter
+  void onUnityMessage(message) {
+    print('Received message from unity: ${message.toString()}');
+  }
+
+  // Callback that connects the created controller to the unity controller
+  void onUnityCreated(controller) {
+    this._unityWidgetController = controller;
+  }
+
+  // Communication from Unity when new scene is loaded to Flutter
+  void onUnitySceneLoaded(SceneLoaded sceneInfo) {
+    print('Received scene loaded from unity: ${sceneInfo.name}');
+    print('Received scene loaded from unity buildIndex: ${sceneInfo.buildIndex}');
+  }
+
 }
